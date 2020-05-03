ea764ccab5629e59390cb00bea4d6908d0e9d1dc
Vincent Agnano <vinyll@protonmail.com>
2020-05-03 13:58:53 +0200

    fixing dependencies for install

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ ROOT=~/.certbot-src
 
 git clone https://github.com/certbot/certbot $ROOT
 cd $ROOT
+sudo apt-get install python3-dev python3-venv gcc libaugeas0 libssl-dev libffi-dev ca-certificates openssl
 python3 tools/venv3.py
 sudo ln -s $ROOT/venv3/bin/certbot /usr/local/bin/
